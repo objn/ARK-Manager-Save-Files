8a5560e3b0ba9e3801d0b08237b7e3ee91afe26d
objn <objn.alpha@gmail.com>
2023-01-23 13:00:05 +0700

    1.0 Beta
    if you found error send report to me please.
    
    enjoy
    23 jan 2023

---
+++ ARK Manager Save Files/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(381, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(101, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(274, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(101, 35)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(274, 23)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(381, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(12, 64)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(444, 200)
+        Me.ListView1.TabIndex = 5
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.List
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "ARK.ico")
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(117, 270)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(99, 30)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Backup All"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(324, 270)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(132, 30)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Clean and Extract"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 270)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(99, 30)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "Backup"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 306)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox3.Size = New System.Drawing.Size(444, 143)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 15)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Folder Savefile"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 15)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Folder Zip"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 457)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(472, 22)
+        Me.StatusStrip1.TabIndex = 14
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 479)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "ARK Manager Save Files 1.0 beta"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+End Class
---
+++ ARK Manager Save Files/Form1.vb
@@ -0,0 +1,265 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.IO.Compression
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox2.Text = FolderBrowserDialog1.SelectedPath
+            update_listview(TextBox2.Text)
+        End If
+    End Sub
+
+    Private Sub update_listview(sourceDirectory As String)
+        If sourceDirectory <> "" Then
+            ' Get a list of all the zip files in the source directory
+            Dim files As String() = Directory.GetFiles(sourceDirectory)
+
+            ' Clear the listview
+            ListView1.Items.Clear()
+
+            ' Loop through each zip file
+            For Each file As String In files
+                If Path.GetExtension(file) = ".zip" Then
+                    ' Add the zip file to the listview
+                    Dim item As New ListViewItem(Path.GetFileNameWithoutExtension(file))
+                    item.SubItems.Add(file)
+                    item.Tag = file
+                    ListView1.Items.Add(item)
+                Else
+
+                End If
+            Next
+        Else
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadSettings()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Set the source and destination directories
+        Dim sourceDirectory As String = TextBox1.Text
+        Dim destinationDirectory As String = TextBox2.Text
+
+        ' Create the destination directory if it does not exist
+        If Not Directory.Exists(destinationDirectory) Then
+            Directory.CreateDirectory(destinationDirectory)
+        End If
+
+        ' Get a list of all the files in the source directory
+        Dim files As String() = Directory.GetFiles(sourceDirectory)
+
+        ' Loop through each file
+        ToolStripProgressBar1.Value = 0
+        ToolStripProgressBar1.Minimum = 0
+        ToolStripProgressBar1.Maximum = files.Length
+        For Each fileselect As String In files
+            savetolog("Processing file " & fileselect)
+            ' Get the file name and extension
+            Dim fileName As String = Path.GetFileName(fileselect)
+            Dim fileExtension As String = Path.GetExtension(fileselect)
+            ' Check if the file is a save file (has a .ark extension)
+            If fileExtension = ".ark" Then
+                ' Create a new zip file with the same name as the save file
+                If File.Exists(TextBox2.Text & fileName & ".zip") Then
+                    savetolog("Zip file already exists")
+                    Dim result As DialogResult = MessageBox.Show("The file already exists. Do you want to replace it?", "File Exists", MessageBoxButtons.YesNo)
+                    If result = DialogResult.Yes Then
+                        savetolog("Deleting zip file")
+                        ' The user wants to replace the file
+                        File.Delete(TextBox2.Text & fileName & ".zip")
+                    Else
+                        ' The user does not want to replace the file
+                        savetolog("Skip zip file")
+                        Continue For
+                    End If
+                End If
+                savetolog("Creating Zip file")
+                Dim zipFilePath As String = Path.Combine(TextBox2.Text, fileName & ".zip")
+                If File.Exists(zipFilePath) Then
+                    File.Delete(zipFilePath)
+                End If
+
+                Using zip As ZipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create)
+                    ' Add the save file to the zip file
+                    zip.CreateEntryFromFile(fileselect, fileName)
+                End Using
+                savetolog("--Successful--")
+            Else
+                savetolog("This file is not ARK SAVE")
+            End If
+            ToolStripProgressBar1.Value += 1
+        Next
+        update_listview(TextBox2.Text)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Loop through each file
+        savetolog("Opening OpenFileDialog")
+        OpenFileDialog1.FileName = ""
+        OpenFileDialog1.Filter = "ARK File (*.ark)|*.ark"
+        OpenFileDialog1.InitialDirectory = TextBox1.Text
+        OpenFileDialog1.Multiselect = True
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            ToolStripProgressBar1.Value = 0
+            ToolStripProgressBar1.Minimum = 0
+            ToolStripProgressBar1.Maximum = OpenFileDialog1.FileNames.Length
+            For Each fileselect As String In OpenFileDialog1.FileNames
+                savetolog("Processing file " & fileselect)
+                ' Get the file name and extension
+                Dim fileName As String = Path.GetFileName(fileselect)
+                Dim fileExtension As String = Path.GetExtension(fileselect)
+                ' Check if the file is a save file (has a .ark extension)
+                If fileExtension = ".ark" Then
+                    ' Create a new zip file with the same name as the save file
+                    If File.Exists(TextBox2.Text & fileName & ".zip") Then
+                        savetolog("Zip file already exists")
+                        Dim result As DialogResult = MessageBox.Show("The file already exists. Do you want to replace it?", "File Exists", MessageBoxButtons.YesNo)
+                        If result = DialogResult.Yes Then
+                            savetolog("Deleting zip file")
+                            ' The user wants to replace the file
+                            File.Delete(TextBox2.Text & fileName & ".zip")
+                        Else
+                            ' The user does not want to replace the file
+                            Continue For
+                        End If
+                    Else
+                    End If
+                    savetolog("Creating Zip file")
+                    Dim zipFilePath As String = Path.Combine(TextBox2.Text, fileName & ".zip")
+                    If File.Exists(zipFilePath) Then
+                        File.Delete(zipFilePath)
+                    End If
+
+                    Using zip As ZipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create)
+                        ' Add the save file to the zip file
+                        zip.CreateEntryFromFile(fileselect, fileName)
+                    End Using
+                    savetolog("--Successful--")
+                End If
+            Next
+            update_listview(TextBox2.Text)
+        End If
+        ToolStripProgressBar1.Value += 1
+    End Sub
+    Sub savetolog(msg As String)
+        TextBox3.AppendText(DateTime.Now.ToString() & ": " & msg & vbCrLf)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim files As String() = Directory.GetFiles(TextBox1.Text)
+        ' Loop through each file
+        ToolStripProgressBar1.Value = 0
+        ToolStripProgressBar1.Minimum = 0
+        ToolStripProgressBar1.Maximum = ListView1.SelectedItems.Count + files.Length
+        For Each delfile As String In files
+            savetolog("Deleting ARK SAVE " & delfile)
+            ' Check if the file has the ".ark" extension
+            If Path.GetExtension(delfile) = ".ark" Then
+                ' Delete the file
+                File.Delete(delfile)
+                savetolog("--Successful--")
+            End If
+            ToolStripProgressBar1.Value += 1
+        Next
+        For Each item As ListViewItem In ListView1.SelectedItems
+            savetolog("Extracting zip file " & item.Tag.ToString)
+            ZipFile.ExtractToDirectory(item.Tag.ToString, TextBox1.Text, True)
+            savetolog("--Successful--")
+            ToolStripProgressBar1.Value += 1
+        Next
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        SaveSettings()
+    End Sub
+    Private Sub LoadSettings()
+        Dim filePath As String = "setting.txt"
+        If File.Exists(filePath) Then
+            Try
+                Using reader As New StreamReader(filePath)
+                    Dim fileContent As String = reader.ReadToEnd()
+                    Dim lines As String() = fileContent.Split(vbCrLf)
+                    For Each line As String In lines
+                        ' Split the line into the key and value
+
+                        Dim keyValue As String() = line.Split(">")
+                        If keyValue.Length = 2 Then
+                            Dim key As String = keyValue(0).Trim()
+                            Dim value As String = keyValue(1).Trim()
+
+                            ' Set the appropriate TextBox value based on the key
+                            Select Case key
+                                Case "SavedArksLocal Directory"
+                                    TextBox1.Text = value
+                                Case "Zip Directory"
+                                    TextBox2.Text = value
+                                Case Else
+                                    ' Ignore any other keys
+                            End Select
+                        End If
+                    Next
+                End Using
+                If String.IsNullOrEmpty(TextBox1.Text) OrElse String.IsNullOrEmpty(TextBox2.Text) Then
+                    Dim result As DialogResult = MessageBox.Show("Setting error you will reset?", "Error", MessageBoxButtons.YesNo)
+                    If result = DialogResult.Yes Then
+                        File.Delete("setting.txt")
+                        GenerateSetting()
+                    Else
+                        Me.Close()
+                    End If
+                Else
+                    update_listview(TextBox2.Text)
+                End If
+
+            Catch ex As Exception
+                ' Handle any exceptions here
+            End Try
+        Else
+            ' File does not exist, create it
+            GenerateSetting()
+        End If
+    End Sub
+
+    Private Sub SaveSettings()
+        If String.IsNullOrEmpty(TextBox1.Text) OrElse String.IsNullOrEmpty(TextBox2.Text) Then
+            ' Display an error message
+            MessageBox.Show("Setting can't save. something wrong", "Error")
+            Return
+        End If
+
+        Dim filePath As String = "setting.txt"
+
+        ' Use a "using" statement to automatically close the file after it is done being used
+        Using writer As StreamWriter = New StreamWriter(filePath)
+            writer.WriteLine("# ARK MANAGER SAVE BY OBJN #")
+            writer.WriteLine("SavedArksLocal Directory > " + TextBox1.Text)
+            writer.WriteLine("Zip Directory > " + TextBox2.Text)
+            writer.WriteLine("#")
+        End Using
+    End Sub
+    Private Sub GenerateSetting()
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            If Directory.Exists(FolderBrowserDialog1.SelectedPath) Then
+                TextBox1.Text = FolderBrowserDialog1.SelectedPath
+                TextBox2.Text = FolderBrowserDialog1.SelectedPath.Split("SavedArksLocal\")(0) & "ZippedSaves"
+                Directory.CreateDirectory(TextBox2.Text)
+                SaveSettings()
+                Application.Restart()
+            Else
+                Me.Close()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
